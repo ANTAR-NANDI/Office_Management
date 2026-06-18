94bffa5c4563bda6ecf181d02f3162edf4937ddb
ANTAR NANDI <antarnandy.2@gmail.com>
2026-06-18 18:19:11 +0600

    Registration Api done

--- backend/server.js
+++ backend/server.js
@@ -22,3 +22,14 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
+const authRoutes = require("./routes/auth.routes");
+
+app.use("/api/auth", authRoutes);
+
+/////////////////////////////////////////
+const User = require("./models/User");
+
+sequelize.sync({ alter: true })
+  .then(() => console.log("DB Synced"))
+  .catch(err => console.log(err));
